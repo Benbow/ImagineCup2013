5a324c3808d97ef535f3e29e516edea5884f1db4
Constant <constant.brunel@supinfo.com>
2013-01-28 15:13:12 -0500

    Commit pour benbow

---
+++ documents/for dev/Jekyll/Jekyll/Jekyll/GameMain.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jekyll
+{
+    class GameMain
+    {
+        int statut_player;
+        Jekyll LocalJekyll;
+        Hide LocalHide;
+
+        public GameMain()
+        {
+            statut_player = 0;
+            LocalJekyll = new Jekyll();
+            LocalHide = new Hide();
+        }
+
+        public void Update(MouseState mouse, KeyboardState keyboard)
+        {
+            if (statut_player == 0)
+            {
+                LocalJekyll.Update(mouse, keyboard);
+                statut_player = LocalJekyll.Switch(keyboard, statut_player);
+            }
+            else if (statut_player == 1)
+            {
+                LocalHide.Update(mouse, keyboard);
+                statut_player = LocalHide.Switch(keyboard, statut_player);
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (statut_player == 0)
+                LocalJekyll.Draw(spriteBatch);
+            else if (statut_player == 1)
+                LocalHide.Draw(spriteBatch);
+        }
+    }
+}
---
+++ documents/for dev/Jekyll/Jekyll/Jekyll/Resources.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jekyll
+{
+    class Resources
+    {
+        public static Texture2D Jekyll;
+        public static Texture2D Hide;
+
+        public static void LoadContent(ContentManager Content)
+        {
+            Jekyll = Content.Load<Texture2D>("guys");
+            Hide = Content.Load<Texture2D>("hide");
+        }
+    }
+}
